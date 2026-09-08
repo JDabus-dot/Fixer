0e9856f2645523284aff7e02c785b70eaefac58a
Marianela <belardinelli.marianela@gmail.com>
2026-09-08 09:13:45 -0300

    Feat: api hola mundo

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Backend inicializado correctamente');
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hola mundo desde Fixer API" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
